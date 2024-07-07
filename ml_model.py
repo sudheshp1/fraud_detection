7c255d22e51638e2757f4fec88af9bc2d2b4334d
sudheshp1 <sudheshat94@gmail.com>
2024-07-07 10:58:02 +0530

    Added 3 new models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ml model code here")
+
+def model1():
+    print("This is a new model")
+
+def model2():
+    print("This is a new model2")
+
+def model13():
+    print("This is a new model3")
